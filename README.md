787e9ca5db71bdce4446204f5c12e6b8cf1830eb
Ariun_Erdene <ariun-erdene@ailab.mn>
2022-10-18 15:03:14 +0800

    [UPDATED] compiled tailwind to make raw css

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 1. Style-уудаа оруулна.
 2. `npm run postcss` кодыг ажиллуулан style > main.scss файл үүсгэнэ.
 3. `sass style/main.scss style/converted/main.css` кодыг ажиллуулан style > converted > main.css файл үүсгэнэ.
+4. `npx tailwindcss -i ./style/converted/main.css -o ./dist/main.css` кодыг ажиллуулан raw css файл бий болгоно. Гэхдээ style > converted > main.css файл дээр `@tailwind base;
+   @tailwind components;
+   @tailwind utilities;` мөрүүд заавал орсон байх ёстой.
+
 
 ## Ашигласан CDN технологи
 [jsdelivr](https://www.jsdelivr.com/)
---
+++ tailwind.config.cjs
@@ -0,0 +1,71 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      boxShadow: {
+        'btn-primary': '0 4px 8px rgba(53, 48, 134, 0.4)',
+        'card-primary': '0 4px 4px rgba(0,0,0,0.05)',
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      background: '#F7F8FA',
+      primary: '#353086',
+      secondary: '#33ACB8',
+      success: '#0D9488',
+      error: '#DC2626',
+      info: '#33BFFF',
+      warning: '#F4BD50',
+      white: '#ffffff',
+      default: '#838383',
+      gray: {
+        50: '#EBEEF2',
+        100: '#DADEE6',
+        200: '#B8BFCC',
+        290: '#9AA4B5',
+        300: '#98A2B3',
+        310: '#f5f6fa',
+        400: '#7A8699',
+        500: '#606C80',
+        600: '#475366',
+        700: '#323C4D',
+        800: '#606C80',
+        900: '#606C80',
+      },
+      black: {
+        300: '#2f3542',
+        400: '#222f3e',
+        500: '#294566',
+        600: '#2d3436',
+        700: '',
+        800: '#2C3A47',
+        900: '#000000',
+      },
+      blue: {
+        100: '#A3B6CC',
+        500: '#54a0ff',
+        600: '#2e86de',
+      },
+      green: {
+        300: '#17E933',
+        400: '#1dd1a1',
+        500: '#2ecc71',
+        600: '#10ac84',
+        700: '#26de81',
+        800: '#20bf6b',
+      },
+      red: {
+        500: '#e74c3c',
+        600: '#e84118',
+      },
+      yellow: {
+        400: '#fbc531',
+        500: '#fed330',
+        600: '#f6b93b',
+        700: '#f7b731',
+      },
+    },
+  },
+  plugins: [],
+}
